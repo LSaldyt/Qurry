76ca9b5e108a313bd6a312bc15cf992b8e6b24d3
LSaldyt <lucassaldyt@gmail.com>
2018-07-08 15:56:23 -0700

    Separates run and compilation scripts

---
+++ run.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import sys
+
+from pyquil.quil  import Program
+from pyquil.api   import QVMConnection
+from pyquil.gates import ALL_GATES
+
+def run(filename):
+    with open(filename, 'r') as infile:
+        quil = infile.read()
+    program = Program(quil)
+    qvm = QVMConnection()
+    wave_function = qvm.wavefunction(program)
+    print(wave_function)
+
+def main(args):
+    assert len(args) > 0, 'Usage: ./run [filename]'
+    filename = args[0]
+    run(filename)
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))
