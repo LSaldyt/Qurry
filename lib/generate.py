2d628326f779dd8100c1cdba2ca6572c419b05bd
LSaldyt <lucassaldyt@gmail.com>
2018-08-15 07:49:09 -0700

    Adds "Clear" macro

--- lib/generate.py
+++ lib/generate.py
@@ -2,7 +2,7 @@ from pyquil.gates import STANDARD_INSTRUCTIONS
 
 from pprint import pprint
 
-from .templates import if_template
+from .templates import if_template, clear_template
 from .utils     import named_uuid
 
 def create_if(cond, a, b):
@@ -12,6 +12,9 @@ def create_if(cond, a, b):
             b=generate_single(b),
             first=named_uuid('first'),
             end=named_uuid('end'))
+
+def create_clear(q, scratch=63):
+    return clear_template.format(q=q, scratch_bit=scratch, uuid=named_uuid('qubit-{}'.format(q)))
 
 def build(stack):
     definitions = dict()
@@ -26,8 +29,10 @@ def build(stack):
     for expression in stack:
         assert len(expression) > 0, 'Error: parsed empty list {}'.format(item)
         head = expression[0]
-        if head in STANDARD_INSTRUCTIONS:
-            if head == 'MEASURE':
+        upper = head.upper()
+        if upper in STANDARD_INSTRUCTIONS:
+            expression[0] = upper
+            if upper == 'MEASURE':
                 yield '{} {} [{}]'.format(*insert_defs(expression))
             else:
                 yield ' '.join(insert_defs(expression))
@@ -36,11 +41,19 @@ def build(stack):
         elif head == 'def':
             assert len(expression) == 3, 'Def expressions should take the form (def var val)'
             definitions[expression[1]] = expression[2]
+        elif head == 'clear':
+            assert len(expression) == 2, 'Clean expressions should look like: (clear q)'
+            yield create_clear(expression[1])
         elif head == 'flip':
             assert len(expression) == 1 or len(expression) == 2, 'Flip requires either zero or one argument'
+            if len(expression) == 1:
+                yield ''
+            else:
+                yield ''
             print(expression)
             yield '# FLIP {}'
         else:
+            print('No generation branch for:')
             print(head)
             1/0
 
--- lib/templates.py
+++ lib/templates.py
@@ -1,7 +1,13 @@
-if_template = '''
+if_template = '''# Conditional statement
 JUMP-WHEN @{first} [{cond}]
   {b}
 JUMP @{end}
 LABEL @{first}
   {a}
 LABEL @{end}'''
+
+clear_template = '''# Clearing qubit {q}
+MEASURE {q} [{scratch_bit}]
+JUMP-UNLESS @{uuid} [{scratch_bit}]
+X {q}
+LABEL @{uuid}'''
