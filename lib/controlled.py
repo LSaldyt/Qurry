61ec0765c6e4da7a8c46b32fcb5064f46aedd5be
LSaldyt <lucassaldyt@gmail.com>
2018-08-17 15:39:53 -0700

    Adds controlled-X

--- lib/controlled.py
+++ lib/controlled.py
@@ -1,4 +1,7 @@
+from pyquil.parameters import Parameter, quil_sin, quil_cos
+from pyquil.quilbase import DefGate
 import pyquil
+
 import numpy as np
 
 def controlled(U):
@@ -10,3 +13,12 @@ def controlled(U):
 Y = np.array([[0, 0.0-1j], [0.0+1j, 0]])
 print(Y)
 print(controlled(Y))
+
+def controlled_X(n):
+    theta = Parameter('theta')
+    crx = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, quil_cos(theta / 2), -1j * quil_sin(theta / 2)], [0, 0, -1j * quil_sin(theta / 2), quil_cos(theta / 2)]])
+    CRX = DefGate('CRX', crx, [theta])
+    print(CRX)
+    return CRX.get_constructor()(n)(0, 1)
+
+print(controlled_X(1.567))
