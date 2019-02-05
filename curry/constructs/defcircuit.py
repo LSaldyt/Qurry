ba4cc1a0163a3b1b7e2d062b9433476cba3783d5
LSaldyt <lucassaldyt@gmail.com>
2019-02-05 12:08:27 -0700

    Adds rudimentary circuit definitions

--- curry/constructs/defcircuit.py
+++ curry/constructs/defcircuit.py
@@ -1,12 +1,16 @@
 from ..utils import named_uuid
+from collections import namedtuple
+
+Circuit = namedtuple('Circuit', ['arguments', 'expressions'])
 
 def defcircuit(name, argdef, expressions, definitions=None, builder=None):
     '''
     Create a repeatable circuit
     '''
-    definitions['a'] = 0
-    definitions['b'] = 1
-    #s = generate(..)
-    del definitions['a']
-    del definitions['b']
-    return ''
+    circuit = Circuit(argdef, expressions)
+
+    def circuit_builder(*args):
+        return '\n'.join(builder(expr) for expr in expressions)
+
+    definitions[name] = circuit_builder
+    return '# This line defines the {} circuit'.format(name)
