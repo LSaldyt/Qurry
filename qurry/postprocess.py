cc5b544f19f6155fcfe7470ac104ad1b5dfdec4e
LSaldyt <lucassaldyt@gmail.com>
2019-08-04 11:54:15 -0700

    Adds dirt-simple post-processing as an example

--- qurry/postprocess.py
+++ qurry/postprocess.py
@@ -2,5 +2,21 @@ from pprint import pprint
 
 from .visualization import histogram
 
+def transform_results(results):
+    assert len(results) > 0, 'Cannot post-process empty results'
+    first = list(results.values())[0]
+
+    trials = dict()
+    for i in range(len(first)):
+        trial_measurement = dict()
+        for qubit, measurements in results.items():
+            trial_measurement[qubit] = measurements[i]
+        trials[i] = trial_measurement
+    return trials
+
 def postprocess(results):
-    pprint(results)
+    results = transform_results(results)
+    for k, v in results.items():
+        print(str(k) + ': ' +  ' '.join(map(str, v.values())))
+    #pprint(results)
+    histogram(results)
--- qurry/visualization/histogram.py
+++ qurry/visualization/histogram.py
@@ -1,3 +1,24 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy import stats
 
-def histogram():
-    return
+from collections import defaultdict
+from pprint import pprint
+
+def histogram(results):
+    counts = defaultdict(lambda : 0)
+    for trial, trial_measurements in results.items():
+        counts[tuple(trial_measurements.values())] += 1
+    pprint(counts)
+    predata = defaultdict(list)
+    for k, v in counts.items():
+        measured = ''.join(map(str, tuple(k)))
+        for _ in range(v):
+            predata['measurement'].append(measured)
+        #predata['count'].append(v)
+    data = pd.DataFrame(predata)
+    pprint(data)
+    sns.countplot(x='measurement', data=data)
+    plt.show()
